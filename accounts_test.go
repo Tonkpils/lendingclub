f408e1a035677f3763755355fb8990d41ea030c6
Leo Correa <lcorr005@gmail.com>
2016-01-26 19:03:01 -0500

    Summary API tests and use testify for assertions

--- accounts_test.go
+++ accounts_test.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 
 	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 var (
@@ -16,30 +18,44 @@ var (
 func TestAvailableCash(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		availableCashAPI := fmt.Sprintf("/accounts/%d/availablecash", TestAccountID)
-		if availableCashAPI != req.RequestURI {
-			t.Errorf("Expected available cash API to be %s, but got %s",
-				availableCashAPI, req.RequestURI)
-		}
+		assert.Equal(t, availableCashAPI, req.RequestURI)
 
-		if err := respondWithFixture(w, "available_cash.json"); err != nil {
-			t.Fatal(err)
-		}
+		err := respondWithFixture(w, "available_cash.json")
+		require.NoError(t, err)
 	}))
 	defer ts.Close()
 
 	ar := newClient(ts.URL, "Token", nil).Accounts(TestAccountID)
 	ac, err := ar.AvailableCash()
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
-	if expected := 12345; ac.InvestorID != expected {
-		t.Errorf("Expected Investor ID to be %d, but got %d",
-			expected, ac.InvestorID)
-	}
+	assert.Equal(t, 12345, ac.InvestorID)
+	assert.Equal(t, decimal.NewFromFloat(100.76), ac.AvailableCash)
+}
 
-	if expected := decimal.NewFromFloat(100.76); !ac.AvailableCash.Equals(expected) {
-		t.Errorf("Expected Available Cash to equal %s, but got %s",
-			expected, ac.AvailableCash)
-	}
+func TestSummary(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		summaryAPI := fmt.Sprintf("/accounts/%d/summary", TestAccountID)
+		assert.Equal(t, summaryAPI, req.RequestURI)
+
+		err := respondWithFixture(w, "summary.json")
+		require.NoError(t, err)
+	}))
+	defer ts.Close()
+
+	ar := newClient(ts.URL, "Token", nil).Accounts(TestAccountID)
+	summary, err := ar.Summary()
+	require.NoError(t, err)
+
+	assert.Equal(t, 1788402, summary.InvestorID)
+	assert.Equal(t, decimal.NewFromFloat(50.77), summary.AvailableCash)
+	assert.Equal(t, decimal.NewFromFloat(100.15), summary.AccountTotal)
+	assert.Equal(t, decimal.NewFromFloat(0.26), summary.AccruedInterest)
+	assert.Equal(t, decimal.NewFromFloat(0), summary.InFundingBalance)
+	assert.Equal(t, decimal.NewFromFloat(0.16), summary.ReceivedInterest)
+	assert.Equal(t, decimal.NewFromFloat(0.62), summary.ReceivedPrincipal)
+	assert.Equal(t, decimal.NewFromFloat(0), summary.ReceivedLateFees)
+	assert.Equal(t, decimal.NewFromFloat(49.38), summary.OutstandingPrincipal)
+	assert.Equal(t, 2, summary.TotalNotes)
+	assert.Equal(t, 3, summary.TotalPortfolios)
 }
